61ab871823dfbc389e5d9bcf2723e8f5f39649e2
Simon-157 <boatengsimonjnr157@gmail.com>
2022-07-25 00:14:35 +0000

    conditionally rendered routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import './App.css';
 import { GlobalStyles } from "./components/styles/GlobalStyled";
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
@@ -9,10 +10,13 @@ import Patients from './pages/service/Patients/Patients';
 import Login from "./pages/auth/login/Login";
 import Consultation from './pages/service/consultation/Consultation';
 import Examination from './pages/service/examination/Examination';
-import UserProvider from "./contexts/userContext";
+import UserProvider, { userContext } from "./contexts/userContext";
 import PageNotFound from './pages/errors/page-not-found/PageNotFound';
 
+
 const App = ()=> {
+
+  const {user} = useContext(userContext)
   return (
   
       <FormProvider>
@@ -22,13 +26,13 @@ const App = ()=> {
       <Router>
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={!user?<Login />:<LandingPage />} />
           <Route path="/health/consultation" element = {<Consultation />} />
           <Route path="/health/pharmacy" />
           <Route path="/accounts" />
-          <Route path="/health/patients" element={<Patients />} />
+          <Route path="/health/patients" element={user?<Patients />:<Login />} />
           <Route path="/report" element = {<dummy /> }/>
-          <Route path="/examination" element = {<Examination />} />
+          <Route path="/examination" element = {user?<Examination />:<Login />} />
           <Route path="*" element = {<PageNotFound />} />
         </Routes>
       </Router>
